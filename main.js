bba2c3949fed2169b73f670b75ad535fc39d97e2
ligitaZA <ligitazaicevaite@gmail.com>
2023-01-04 15:04:12 +0200

    prideta data.json ir JS

--- main.js
+++ main.js
@@ -51,3 +51,32 @@ fetch('data.json')
       videosDiv.appendChild(video);
     }
   });
+
+  const subscriptionsDiv = document.querySelector('.subscriptions');
+
+const createSubscriptionElement = (data) => {
+  const subscriptionDiv = document.createElement('div');
+  subscriptionDiv.classList.add('subscription');
+  
+  const img = document.createElement('img');
+  img.src = data.imageUrl;
+  img.alt = "";
+  img.style.height = "22px";
+  
+  const nameSpan = document.createElement('span');
+  nameSpan.textContent = data.name;
+  
+  subscriptionDiv.appendChild(img);
+  subscriptionDiv.appendChild(nameSpan);
+  
+  return subscriptionDiv;
+}
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    for (const subscriptionData of data.subscriptions) {
+      const subscription = createSubscriptionElement(subscriptionData);
+      subscriptionsDiv.appendChild(subscription);
+    }
+  });
